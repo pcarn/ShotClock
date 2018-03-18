f85e6a2b61d3c7f04fe4834d993b46541ec7873e
Peter Carnesciali <pcarn9@gmail.com>
2018-03-18 10:44:41 -0500

    Initial Commit

---
+++ Shot Clock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shot Clock
+//
+//  Created by Peter Carnesciali on 3/18/18.
+//  Copyright © 2018 Peter Carnesciali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
